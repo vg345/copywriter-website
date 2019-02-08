60390db598db9ef719e4e43cf04a427d5f652c42
Vanshika Gulati <vanshikagulati@Vanshikas-MacBook-Air.local>
2019-02-08 18:11:30 +0530

    Update

--- js/submit.js
+++ js/submit.js
@@ -31,5 +31,26 @@ function validation() {
 const fs = require('fs');
 
 function submit_by_id() {
-  
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let mess = document.getElementById("message").value;
+  let message = name + '\t' + email + '\t' + mess + '\n';
+  if (validate()) {
+    fs.writeFile('messages.txt', message, (err)=> if (err) throw err; )
+  }
+}
+
+function validate() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let mess = document.getElementById("message").value;
+  let emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+  if (name === '' || email === '' || message = '') {
+    alert("Please fill all fields.");
+    return false;
+  } else if (!(email).match(emailReg)) {
+    alert("Invalid Email.");
+    return false;
+  } else {
+    return true;
 }
